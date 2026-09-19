97615c355aca95f2573fb91e67261788ebd20627
dimple <dimple13619saini@gmail.com>
2026-09-19 21:39:19 +0530

    Configure backend for deployment

--- server/server.js
+++ server/server.js
@@ -22,6 +22,6 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Server running on port 8080"); 
 });
